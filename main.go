0845bacd551ee7e99d7a795f5b9398f12aa182c9
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-01-22 10:50:17 -0500

    added isPalindrome leetcode solution

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Solution struct {}
+
+func (s *Solution) checkPalindrome(number int) bool {
+	// abs value of number - base case
+	math.Abs(float64(number));
+
+	originalNum := number
+
+	reverseNum := 0
+
+	for number != 0 {
+		// Get the last digit and then add to a temporary
+		lastDigit := number % 10
+		reverseNum = reverseNum * 10 + lastDigit
+		number = number / 10
+	}
+
+	return reverseNum == originalNum
+}	
 
 func main() {
 	// Leet-Code Problem - isPalindrome
-	var i int
-    fmt.Print("Enter a Number: ")
-	fmt.Scan(&i)
+	solution_obj := &Solution{}
 
-	fmt.Println("Your number is:", i * 10)
+	isPalindrome := solution_obj.checkPalindrome(202)
+	
+	fmt.Println("Is a palindrome?:", isPalindrome)
 }
