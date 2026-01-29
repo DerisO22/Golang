af3310c88d560a8e0ed71d4f26987009af448eb7
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-01-29 11:37:39 -0500

    Got the slice loaded with messages from the txt (PA1)

--- PAs/PA1/pa1.go
+++ PAs/PA1/pa1.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+const FILE_TO_READ = "messages.txt"
+
+// reads and add to list
+func readFromFile(messageArray *[]string) {
+	// From Lab 3 Example of Reading File
+	readFile, err := os.Open(FILE_TO_READ);
+	
+	if err != nil {
+		fmt.Println("Error opening file:", err);
+		return;
+	}
+	
+	defer readFile.Close();
+
+	scanner := bufio.NewScanner(readFile);
+	fmt.Println("Reading from file:");
+
+	i := 0;
+
+	for scanner.Scan() {
+		*messageArray = append(*messageArray, scanner.Text());
+		fmt.Println((*messageArray)[i]);
+		i++;
+	}
+}
+
+// generate rand num
+func generateRandNumber(max_num int) int {
+	// from lab 3 specifications
+	max := big.NewInt(int64(max_num));
+
+	tmp, err := rand.Int(rand.Reader, max);
+
+	if err != nil {
+		panic(err);
+	}
+
+	return int(tmp.Int64());
+}
 
 func main() {
-	fmt.Println("Hello Bro")
+	// using slice like the generateArray func from lab3 example
+	var messages []string;
+
+	// I could just return the new populated message slice
+	// in readFromFile, but wanted to try out pointers/pass-by-ref
+	readFromFile(&messages);
+
+
 }
